d23a762fb32a8c3da8b8a03a4c0ca75d65d5cd74
dev-seony <tjsdud9113@gmail.com>
2026-05-23 20:12:17 +0900

    Update LinkRequestDTO.java

--- backend/src/main/java/com/example/backend/Model/DTO/LinkRequestDTO.java
+++ backend/src/main/java/com/example/backend/Model/DTO/LinkRequestDTO.java
@@ -2,13 +2,11 @@ package com.example.backend.Model.DTO;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
 import lombok.Setter;
-
+@NoArgsConstructor
 @Getter
 @Setter
+
 public class LinkRequestDTO {
     private Integer protectorId;
     private String patientCode;
