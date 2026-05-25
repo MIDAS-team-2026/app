d54d381d69dbf5871e2aae5a2a30f5e085b43126
bugi <ray060402@gmail.com>
2026-05-26 04:37:48 +0900

    네트워크 통신용 WebConfig 추가

--- backend/src/main/java/com/example/backend/Model/DTO/auth/SignupRequestDTO.java
+++ backend/src/main/java/com/example/backend/Model/DTO/auth/SignupRequestDTO.java
@@ -1,7 +1,7 @@
 package com.example.backend.Model.DTO.auth;
 
 public class SignupRequestDTO {
-    private String email;
+    private String phone;
     private String password;
     private String name;
     private String role;
@@ -9,8 +9,8 @@ public class SignupRequestDTO {
     private int gender;
 
     // Getter, Setter
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
     public String getName() { return name; }
