57da612d4318d116782edfc58c032a7cda9b5a93
gamppe <felix0703@naver.com>
2026-06-11 20:59:53 +0900

    [수정] BASE_URL localhost로 변경 (10.0.2.2:8080)

--- app/src/main/java/com/midas26/mobileapp/network/RetrofitClient.kt
+++ app/src/main/java/com/midas26/mobileapp/network/RetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://mogumogu.p-e.kr:8080/"
+    private const val BASE_URL = "http://10.0.2.2:8080/"
 
     private var tokenProvider: (() -> String?)? = null
     private var onUnauthorized: (() -> Unit)? = null
